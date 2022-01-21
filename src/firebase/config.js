0e5f9e2f33f253b1805031f94c65f7eb5f487075
Aidan Dang <aidandang@gmail.com>
2022-01-21 05:49:44 -0800

    setup tailwindcss

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_APIKEY,
+	authDomain: process.env.FIREBASE_AUTHDOMAIN,
+	projectId: process.env.FIREBASE_PROJECTID,
+	storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+	appId: process.env.FIREBASE_APPID,
+};
+
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const db = getFirestore();
+
+export { auth, db };
